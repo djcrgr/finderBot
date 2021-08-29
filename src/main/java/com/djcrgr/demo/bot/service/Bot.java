1ba4ff399951f843920650bcc78677bc692228c0
gerbulov <gerbulov@gmail.com>
2021-08-29 23:22:15 +0300

    finish

--- src/main/java/com/djcrgr/demo/bot/service/Bot.java
+++ src/main/java/com/djcrgr/demo/bot/service/Bot.java
@@ -1,12 +1,28 @@
 package com.djcrgr.demo.bot.service;
 
 import com.djcrgr.demo.bot.config.BotConfig;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -28,6 +44,7 @@ public class Bot extends TelegramLongPollingBot {
         return botConfig.getToken();
     }
 
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
@@ -52,7 +69,28 @@ public class Bot extends TelegramLongPollingBot {
                 log.debug(e.toString());
             }
         }
+        if (messageText.contains("/def")) {
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpGet httppost = new HttpGet("http://ya.ru");
 
+
+// Request parameters and other properties.
+
+//Execute and get the response.
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                try (InputStream instream = entity.getContent()) {
+                    String text = new BufferedReader(
+                            new InputStreamReader(instream, StandardCharsets.UTF_8))
+                            .lines()
+                            .collect(Collectors.joining("\n"));
+                    builder.text("some text : "+ text.substring(0, 100));
+                    execute(builder.build());
+                }
+            }
+        }
         if (messageText.contains("/chartId")) {
             builder.text("ID Канала : " + chatId);
             try {
